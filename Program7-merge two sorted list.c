a61f4fd5786ed599a164dab477f86cb13a318a47
Anand Sadasivan <139990619+Anand-29@users.noreply.github.com>
2024-01-09 07:51:33 +0530

    Update Program7-merge two sorted list.c

--- Program7-merge two sorted list.c
+++ Program7-merge two sorted list.c
@@ -4,69 +4,44 @@ struct node{
     int data;
     struct node *next;
 }*head1,*head2,*head3;
-void insert1(int d){
+struct node *newnode(int d){
     struct node *nn=malloc(sizeof(struct node));
     nn->data=d;
     nn->next=NULL;
-    if(head1==NULL)
-    head1=nn;
+    return nn;
+}
+void insert(int d,struct node *head){
+    if(head==NULL)
+    head=newnode(d);
     else{
         struct node *t=head1;
-        while(t->next!=NULL)
+        while(t->next!=NULL){
         t=t->next;
-        t->next=nn;
+        }
+        t->next=newnode(d);
     }
 }
-void insert2(int d){
-    struct node *nn=malloc(sizeof(struct node));
-    nn->data=d;
-    nn->next=NULL;
-    if(head2==NULL)
-    head2=nn;
-    else{
-        struct node *t=head2;
-        while(t->next!=NULL)
-        t=t->next;
-        t->next=nn;
-    }
-}
-void insert3(int d){
-    struct node *nn=malloc(sizeof(struct node));
-    nn->data=d;
-    nn->next=NULL;
-    if(head3==NULL)
-    head3=nn;
-    else{
-        struct node *t=head3;
-        while(t->next!=NULL)
-        t=t->next;
-        t->next=nn;
-    }
-}
-void mergetwolist(){
-    struct node *t1=head1;
-    struct node *t2=head2;
+void mergetwolist(struct node *t1,struct node *t2){
     while(t1!=NULL && t2!=NULL){
         if(t1->data<=t2->data){
-            insert3(t1->data);
+            insert(t1->data,head3);
             t1=t1->next;
         }
        else{
-            insert3(t2->data);
+            insert(t2->data,head3);
             t2=t2->next;
         }  
     }
     while(t1!=NULL){
-        insert3(t1->data);
+        insert(t1->data,head3);
         t1=t1->next;
     }
     while(t2!=NULL){
-        insert3(t2->data);
+        insert(t2->data,head3);
         t2=t2->next;
     }
 }
-void display(){
-    struct node *t=head3;
+void display(struct node *t){
     while(t!=NULL){
         printf("%d " ,t->data);
         t=t->next;
@@ -74,16 +49,35 @@ void display(){
 }
 int main(){
     head1=NULL,head2=NULL,head3=NULL;
-    insert1(10);
-    insert1(11);
-    insert1(12);
-    insert1(13);
-    insert1(14);
-    insert2(15);
-    insert2(16);
-    insert2(17);
-    insert2(18);
-    insert2(19);
-    mergetwolist();
-    display();
+    while(1){
+        printf("press 1 to enter the data in head1\n");
+        printf("press 2 to enter the data in head2\n");
+        printf("press 3 to merge the lists\n");
+        printf("press 4 to display the merged list\n");
+        int a;
+        scanf("%d",&a);
+        switch(a){
+            case 1:
+            printf("Enter the data\n");
+            scanf("%d",&a);
+            insert(a,head1);
+            break;
+            case 2:
+            printf("enter the data\n");
+            scanf("%d",&a);
+            insert(a,head2);
+            break;
+            case 3:
+            printf("elements merged\n");
+            mergetwolist(head1,head2);
+            break;
+            case 4:
+            printf("the merged list is\n");
+            display(head3);
+            break;
+            case 5:
+            return 0;
+        }
+    }
+    
 }
